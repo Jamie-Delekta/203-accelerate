d082f2d59343e47a15712f29be83ba9a383c0cf9
Jamie Delekta <j-delekta@outlook.com>
2021-02-26 22:04:23 -0500

    Added back to work nav in footer

--- wp-content/themes/accelerate-theme/footer.php
+++ wp-content/themes/accelerate-theme/footer.php
@@ -13,12 +13,15 @@
 		</div><!-- #main -->
 
 		<footer id="colophon" class="site-footer clearfix" role="contentinfo">
+		<div class="navigation-container">
+		<div class="left">&larr;<a href="">back to work</a></div>
+		</div>	
 			<div class="site-info">
 				<div class="site-description">
 					<p><?php bloginfo('description'); ?></p>
 					<p>&copy; <?php bloginfo('title'); ?>, LLC
 				</div>
-
+		
 				<nav class="social-media-navigation" role="navigation">
 					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
 				</nav>
